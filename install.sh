2f4be3343f6b6f9ec63572f7a3b41aba370db4a2
Hugo Sibony <hugo.sibony@epita.fr>
2023-04-21 11:32:12 +0800

    Changed zsh to txt & Added color

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+
+# Color codes for output messages
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Function to display error messages
 display_error() {
-  echo "[Error]: $1"
+  echo -e "${RED}[Error]: $1${NC}"
   exit 1
 }
 
 # Function to display success messages
 display_success() {
-  echo "[Success]: $1"
+  echo -e "${GREEN}[Success]: $1${NC}"
 }
 
 # Check if script is run with root privileges
@@ -26,16 +32,18 @@ cp target/release/aliasmanager /usr/local/bin/ || display_error "Failed to copy 
 user_shell=$(basename "$SHELL")
 echo "Detected user shell: $user_shell"
 # Check if the source command already exists in the appropriate shell rc file
-if grep -q ".my_aliases.zsh" "/home/$SUDO_USER/.$user_shell"rc; then
-  display_success ".my_aliases.zsh already sourced in .$user_shell"rc
+if grep -q ".my_aliases.txt" "/home/$SUDO_USER/.$user_shell"rc; then
+  display_success ".my_aliases.txt already sourced in .$user_shell"rc
 else
-  # Append a source command to the appropriate shell rc file for .my_aliases.zsh
+  # Append a source command to the appropriate shell rc file for .my_aliases.txt
+  touch /home/$SUDO_USER/.my_aliases.txt || display_error "Failed to create .my_aliases.txt"
   echo "" >> "/home/$SUDO_USER/.$user_shell"rc
   echo "# Alias Management Tool" >> "/home/$SUDO_USER/.$user_shell"rc
-  echo "source /home/$SUDO_USER/.my_aliases.zsh" >> "/home/$SUDO_USER/.$user_shell"rc
+  echo "source /home/$SUDO_USER/.my_aliases.txt" >> "/home/$SUDO_USER/.$user_shell"rc
+  display_success "Added source command to /home/$SUDO_USER/.$user_shell"rc
 fi
-
 
 # Print installation success message
 display_success "Installation complete!"
 display_success "You can now use 'aliasmanager' to manage your aliases."
+
